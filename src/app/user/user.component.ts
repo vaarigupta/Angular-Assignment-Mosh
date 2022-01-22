3cebc295171c7be8e9df643c6f05105ad549a941
Vaari Gupta <vaarigupta24@gmail.com>
2022-01-22 15:58:48 +0530

    two way binding

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  
+  username: string="";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+
+  OnEnterUsername(event:Event)
+  {
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+
+}
