aa431ea1b071a1da4724294eb0e8f157ebcf9aff
Vaari Gupta <vaarigupta24@gmail.com>
2022-03-12 17:39:32 +0530

    deleted new files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Assignment2-Practicing-DataBinding';
+  title = 'Angular Assignments';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+   
   ],
   imports: [
     BrowserModule,
